1d7d008c1dc951b71f9603d5929b70de3510c28e
demian_now <demian.now@yandex.ru>
2021-10-27 12:24:48 +0300

    Formatting code

--- src/main/kotlin/Lab1.kt
+++ src/main/kotlin/Lab1.kt
@@ -1,8 +1,7 @@
-fun main()
-{
+fun main() {
     val test = """
         |Я делал эту лабу вроде бы и два дня, но как бы три, хотя чистого времени ушло примерно часа 3""".trimMargin()
-    println(alignRight(test,20))
+    println(alignRight(test, 20))
     println(alignLeft(test, 20))
     println(alignCenter(test, 20))
     println(alignWidth(test, 20))
@@ -16,8 +15,12 @@ enum class Alignment {
     JUSTIFY
 }
 
-fun alignText(text: String, lineWidth: Int, alignment: Alignment): String {
-    return when(alignment){
+fun alignText(
+    text: String,
+    lineWidth: Int,
+    alignment: Alignment
+): String {
+    return when (alignment) {
         Alignment.LEFT -> alignLeft(text, lineWidth)
         Alignment.RIGHT -> alignRight(text, lineWidth)
         Alignment.CENTER -> alignCenter(text, lineWidth)
@@ -26,27 +29,30 @@ fun alignText(text: String, lineWidth: Int, alignment: Alignment): String {
 }
 
 
-fun makeString(lines: List<String>): String{
+fun makeString(
+    lines: List<String>
+): String {
     var result = ""
-    for(i in lines) result = result +  i + "\n"
+    for (i in lines) result = result + i + "\n"
     return result
 }
 
-fun splitByWidth(rawString: String, length: Int): MutableList<String>
-{
-    if(length<=0)
+fun splitByWidth(
+    rawString: String,
+    length: Int
+): MutableList<String> {
+    if (length <= 0)
         throw IllegalArgumentException("Length can't be 0 or negative") //as a precaution
     val listOfLines = rawString.lines().toMutableList()
     var i = 0
-    while(i<listOfLines.size)
-    {
-        if(listOfLines[i].length>length) //here the words are transferred to another line, taking into account punctuation marks
+    while (i < listOfLines.size) {
+        if (listOfLines[i].length > length) //here the words are transferred to another line, taking into account punctuation marks
         {
             val firstPart = listOfLines[i].substring(0, length)
             var secondPart = listOfLines[i].substring(length)
-            secondPart = if(firstPart.contains(' ')) firstPart.substringAfterLast(' ') + secondPart else secondPart
+            secondPart = if (firstPart.contains(' ')) firstPart.substringAfterLast(' ') + secondPart else secondPart
             listOfLines[i] = firstPart.substringBeforeLast(' ')
-            listOfLines.add(i+1, secondPart)
+            listOfLines.add(i + 1, secondPart)
         }
         listOfLines[i] = listOfLines[i].trim()
         i++
@@ -54,7 +60,10 @@ fun splitByWidth(rawString: String, length: Int): MutableList<String>
     return listOfLines
 }
 
-fun alignRight(rawString: String, length: Int): String {
+fun alignRight(
+    rawString: String,
+    length: Int
+): String {
     val listOfLines = splitByWidth(rawString, length)
     for (i in 0 until listOfLines.size) {
         val count = length - listOfLines[i].length + 1
@@ -63,40 +72,48 @@ fun alignRight(rawString: String, length: Int): String {
     return makeString(listOfLines)
 }
 
-fun alignLeft(rawString: String, length: Int): String {
-    val listOfLines = splitByWidth(rawString, length)
-    return makeString(listOfLines)
-}
+fun alignLeft(
+    rawString: String,
+    length: Int,
+): String = makeString(splitByWidth(rawString, length))
 
-fun alignWidth(rawString: String, length: Int): String{
+fun alignWidth(
+    rawString: String,
+    length: Int
+): String {
     val listOfLines = splitByWidth(rawString, length)
-    for(i in 0 until listOfLines.size-1) { //when the text is aligned to the width, the last line does not change
+    for (i in 0 until listOfLines.size - 1) { //when the text is aligned to the width, the last line does not change
         val count = listOfLines[i].countOfSym(' ')
         val countOfSpaces = length - listOfLines[i].length
-        if(count!=0 && countOfSpaces!=0)
-        {
-            val tempCount1 = (count+countOfSpaces)/count+1
-            val tempCount2 = count - countOfSpaces%count
+        if (count != 0 && countOfSpaces != 0) {
+            val tempCount1 = (count + countOfSpaces) / count + 1
+            val tempCount2 = count - countOfSpaces % count
             listOfLines[i] = listOfLines[i].replace(" ", "".padStart(tempCount1, ' '))
-            for(j in 0 until tempCount2)
-                listOfLines[i] = listOfLines[i].replaceFirst("".padStart(tempCount1, ' '), "".padStart(tempCount1-1, ' '))
+            for (j in 0 until tempCount2)
+                listOfLines[i] =
+                    listOfLines[i].replaceFirst("".padStart(tempCount1, ' '), "".padStart(tempCount1 - 1, ' '))
         }
     }
     return makeString(listOfLines)
 }
 
-private fun String.countOfSym(symbol: Char): Int {
-    var count =0
-    for(i in this)
-        if(i==symbol) count++
+private fun String.countOfSym(
+    symbol: Char
+): Int {
+    var count = 0
+    for (i in this)
+        if (i == symbol) count++
     return count
 }
 
-fun alignCenter(rawString: String, length: Int): String{
+fun alignCenter(
+    rawString: String,
+    length: Int
+): String {
     val listOfLines = splitByWidth(rawString, length)
     for (i in 0 until listOfLines.size) {
         val count = length - listOfLines[i].length
-        listOfLines[i] = "".padStart(count/2, ' ') + listOfLines[i].trim() + "".padStart(count/2, ' ')
+        listOfLines[i] = "".padStart(count / 2, ' ') + listOfLines[i].trim() + "".padStart(count / 2, ' ')
     }
     return makeString(listOfLines)
 }

6d1267554698c7399eddd7ac0680104018f360aa
demian_now <demian.now@yandex.ru>
2021-11-23 19:21:54 +0300

    Added lab3, implemented classes without tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.21")
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -20,5 +20,18 @@ fun main() {
    println(hardcore + "=" + test2.calculate(hardcore))
    //всё-таки MutableList в моей реализации необходим, потому что я добавляю и удаляю элементы из списка
    //и реализовать мой алгоритм с помощью создания новых изменённых списков было бы крайне затруднительно
+
+   val listOfShape = mutableListOf<Shape>()
+   for(i in 0..9)
+      listOfShape.add(ShapeFactoryImpl().createRandomShape())
+   listOfShape.forEach {
+      when(it){
+         is Circle -> print("Circle with ")
+         is Square -> print("Square with ")
+         is Rectangle -> print("Rectangle with ")
+         is Triangle -> print("Triangle with ")
+      }
+      println("area: ${it.calcArea()}, perimeter: ${it.calcPerimeter()}")
+   }
 }
 

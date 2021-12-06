b24308cda85dfd0f3053a5b00050010f98078835
demian_now <demian.now@yandex.ru>
2021-12-06 23:44:43 +0300

    Added the simplest operations on matrices

--- src/main/kotlin/Lab4.kt
+++ src/main/kotlin/Lab4.kt
@@ -1,18 +1,26 @@
-open class Matrix(sample: Array<Array<Double>>)
-{
+fun main() {
+    val a = MutableMatrix(arrayOf(arrayOf(1.0)))
+    a /= 2.0
+    a.cols
+    a.rows
+    a[1,0] = 2.0
+}
+
+
+open class Matrix(sample: Array<Array<Double>>) {
     protected var array = arrayOf(arrayOf(1.0))
 
-    init{
+    init {
         array = sample.clone()
         val control = array[0].size
-        for(i in array)
-            if(i.size!=control) throw IllegalArgumentException("The rows of the matrix have different dimensions")
+        for (i in array)
+            if (i.size != control) throw IllegalArgumentException("The rows of the matrix have different dimensions")
     }
 
-    protected var rows = array.size
-        get() = field
-    protected var cols = array[0].size
-        get() = field
+    val rows: Int
+        get() = array.size
+    val cols: Int
+        get() = array[0].size
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -21,69 +29,128 @@ open class Matrix(sample: Array<Array<Double>>)
         if (!array.contentDeepEquals(other.array)) return false
         return true
     }
+
     override fun hashCode(): Int {
         return array.contentDeepHashCode()
     }
+
     override fun toString(): String {
-        return "Matrix(array=${array.contentToString()})"
+        return array.contentToString()
     }
 
     operator fun get(i: Int, j: Int): Double {
-        TODO("Not yet implemented")
+        return array[i][j]
     }
+
     operator fun plus(other: Matrix): Matrix {
-        TODO()
-    }
-    operator fun minus(other: Matrix): Matrix{
-        TODO()
-    }
-    operator fun times(other: Matrix): Matrix {
-        TODO()
-    }
-    operator fun plus(scalar: Double): Matrix {
-        TODO()
-    }
-    operator fun minus(scalar: Double): Matrix{
-        TODO()
-    }
-    operator fun times(scalar: Double): Matrix {
+        checkForSimple(other)
         TODO()
     }
 
-    fun checkForMultiply(other: Matrix){
-        if(this.cols != other.rows)
+    operator fun minus(other: Matrix): Matrix {
+        checkForSimple(other)
+        TODO()
+    }
+
+    operator fun times(other: Matrix): Matrix {
+        checkForMultiply(other)
+        TODO()
+    }
+
+    operator fun plus(scalar: Double): Matrix {
+        val result = array.clone()
+        for(i in result.indices)
+           for(j in result[0].indices)
+               result[i][j]+=scalar
+        return Matrix(result)
+    }
+
+    operator fun minus(scalar: Double): Matrix {
+        val result = array.clone()
+        for(i in result.indices)
+            for(j in result[0].indices)
+                result[i][j]-=scalar
+        return Matrix(result)
+    }
+
+    operator fun times(scalar: Double): Matrix {
+        val result = array.clone()
+        for(i in result.indices)
+            for(j in result[0].indices)
+                result[i][j]*=scalar
+        return Matrix(result)
+    }
+
+    operator fun div(scalar: Double): Matrix {
+        if(scalar==0.0) {
+            throw IllegalArgumentException("Сan't be divided by zero")
+        }
+        val result = array.clone()
+        for(i in result.indices)
+            for(j in result[0].indices)
+                result[i][j]/=scalar
+        return Matrix(result)
+    }
+
+    private fun checkForMultiply(other: Matrix) {
+        if (this.cols != other.rows)
             throw IllegalArgumentException("These matrices have inappropriate dimensions")
     }
 
-    fun checkForUnary(other: Matrix){
-        if(this.cols != other.cols || this.rows != other.rows)
+    private fun checkForSimple(other: Matrix) {
+        if (this.cols != other.cols || this.rows != other.rows)
             throw IllegalArgumentException("These matrices have inappropriate dimensions")
     }
 }
 
-class MutableMatrix(sample: Array<Array<Double>>): Matrix(sample) {
+class MutableMatrix(sample: Array<Array<Double>>) : Matrix(sample) {
     operator fun plusAssign(other: Matrix) {
         TODO()
     }
+
     operator fun minusAssign(other: Matrix) {
         TODO()
     }
-    operator fun timesAssign(other: Matrix){
+
+    operator fun timesAssign(other: Matrix) {
         TODO()
     }
+
     operator fun plusAssign(scalar: Double) {
-        TODO()
+        for(i in array.indices)
+            for(j in array[0].indices)
+                array[i][j]+=scalar
     }
-    operator fun minusAssign(scalar: Double){
-        TODO()
+
+    operator fun minusAssign(scalar: Double) {
+        for(i in array.indices)
+            for(j in array[0].indices)
+                array[i][j]-=scalar
     }
-    operator fun timesAssign(scalar: Double){
-        TODO()
+
+    operator fun timesAssign(scalar: Double) {
+        for(i in array.indices)
+            for(j in array[0].indices)
+                array[i][j]*=scalar
     }
-    operator fun unaryMinus(){
-        TODO()
+
+    operator fun divAssign(scalar: Double) {
+        if(scalar==0.0) {
+            throw IllegalArgumentException("Сan't be divided by zero")
+        }
+        for(i in array.indices)
+            for(j in array[0].indices)
+                array[i][j]/=scalar
     }
-    operator fun unaryPlus(){
-        TODO()
+
+    operator fun unaryMinus() {
+        this.timesAssign(-1.0)
+    }
+
+    operator fun unaryPlus() = //let's imagine that there is at least some practical benefit in this function
+        Unit
+
+    operator fun set(i: Int, j: Int, value: Double) {
+        array[i][j] = value
     }
 }
